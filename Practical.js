5978cb759a1fffad5176b8cc48f60ca22208c80b
Bokang214 <lenkoanebokang214@gmail.com>
2025-09-19 00:38:28 +0100

    activity
    activity

--- Practical.js
+++ Practical.js
@@ -1,2 +1,19 @@
+//Question 1
 var course = "JavaScript Basics";
 alert("Welcome to " + course);
+
+//Question 2
+var name = prompt("What is your name?");
+alert("Hello " + name + ", nice to meet ya!");
+
+//Question 3
+var age = prompt("How old are you?");
+alert("You are " + age + " years old.");
+
+//Question 4
+var num1 = prompt("Enter a number:");
+var num2 = prompt("Enter a number:");
+var sum = Number(num1) + Number(num2);
+alert("The sum of " + num1 + " and " + num2 + " is " + sum + ".");
+
+//Question 5
